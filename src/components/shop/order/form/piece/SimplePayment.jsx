b768a5b10a175c1fbc858d4c07e122ed062eb4f7
[Hanul] <[aouoa03@lomen.kr]>
2024-05-30 18:12:54 +0900

    save

--- src/components/shop/order/form/piece/SimplePayment.jsx
+++ src/components/shop/order/form/piece/SimplePayment.jsx
@@ -2,11 +2,27 @@ import { Button } from "@/components/ui/button";
 import React from "react";
 
 const SimplePayment = () => {
+  const [selectedSimplePayment, setSelectedSimplePayment] = React.useState("");
   return (
     <div>
-      <Button className="bg-green-600 w-full">NAVER</Button>
-      <Button className="bg-yellow-400 w-full mt-2">KAKAO</Button>
-      <Button className="bg-blue-500 w-full mt-2">TOSS</Button>
+      <Button
+        className="bg-green-600 w-full"
+        onClick={() => setSelectedSimplePayment("naver")}
+      >
+        NAVER
+      </Button>
+      <Button
+        className="bg-yellow-400 w-full mt-2"
+        onClick={() => setSelectedSimplePayment("kakao")}
+      >
+        KAKAO
+      </Button>
+      <Button
+        className="bg-blue-500 w-full mt-2"
+        onClick={() => setSelectedSimplePayment("")}
+      >
+        TOSS
+      </Button>
     </div>
   );
 };
